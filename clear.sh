745b93c6d8b57eb1ca775b134a0ae40c4f422228
Quan Chen <chenq182@sina.com>
2018-01-23 21:17:18 +0800

    2018-01-23 21:17:18

--- clear.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-
-rm org_Hello.h
-rm *.o *.so *.class */*.class
--- run.sh
+++ run.sh
@@ -1,12 +1,10 @@
-#!/bin/bash
-
-# apt-get install gcc
-# apt-get install g++
-
+sudo apt-get update
+sudo apt-get -y install gcc g++
+git clone https://github.com/chenq182/jni
+cd jni
 javah org.Hello
-gcc -I/usr/share/java/include -I/usr/share/java/include/linux -fPIC -c Hello.cpp
+gcc -I${JAVA_HOME}/include -I${JAVA_HOME}/include/linux -fPIC -c Hello.cpp
 gcc -shared -Wl,-soname,libhello.so.1 -o libhello.so Hello.o
 export LD_LIBRARY_PATH=`pwd`:
 javac ToSay.java
-
 java ToSay
