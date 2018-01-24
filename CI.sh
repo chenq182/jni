39f049f36d1479e3ab0fe49e1df1b7c76fad3bbd
Quan Chen <chenq182@sina.com>
2018-01-24 10:09:04 +0800

    2018-01-24 10:10:00

--- CI.sh
+++ CI.sh
@@ -1,7 +1,5 @@
 sudo apt-get update
 sudo apt-get -y install gcc g++
-git clone https://github.com/chenq182/jni
-cd jni
 javah org.Hello
 gcc -I${JAVA_HOME}/include -I${JAVA_HOME}/include/linux -fPIC -c Hello.cpp
 gcc -shared -Wl,-soname,libhello.so.1 -o libhello.so Hello.o
